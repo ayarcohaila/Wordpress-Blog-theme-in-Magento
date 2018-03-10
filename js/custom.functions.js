374d20e04da86d5b5d8c8a3532af9694e85613a4
Keith Becker <upworkbecker@gmail.com>
2018-03-09 23:25:47 -0600

    add link to index page on sidebar title

--- js/custom.functions.js
+++ js/custom.functions.js
@@ -1,7 +1,21 @@
+
+//add animation to header of blog page
+
 $(window).scroll(function() {
   if ($(document).scrollTop() > 100) {
     $('#header').addClass('sticky');
   } else {
     $('#header').removeClass('sticky');
   }
+});
+
+
+// add link to sidebar title
+
+
+$(function() {
+  $(".widget-title").css('cursor', 'pointer');
+  $(".widget-title").click(function() {
+    window.location = "/";
+  });
 });
